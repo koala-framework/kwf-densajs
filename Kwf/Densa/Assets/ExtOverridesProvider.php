29b1b09a82d78e01a30d0696f2e5798672604b39
Niko Sams <ns@vivid-planet.com>
2014-08-05 15:08:07 +0200

    fix path to vendor

--- Kwf/Densa/Assets/ExtOverridesProvider.php
+++ Kwf/Densa/Assets/ExtOverridesProvider.php
@@ -45,10 +45,10 @@ class Kwf_Densa_Assets_ExtOverridesProvider extends Kwf_Assets_Provider_Abstract
         static $ret;
         if (isset($ret)) return $ret;
         $ret = array();
-        $it = new RecursiveIteratorIterator(new RecursiveDirectoryIterator(VENDOR_PATH.'/koala-framework/densajs/src/overrides'), RecursiveIteratorIterator::LEAVES_ONLY);
+        $it = new RecursiveIteratorIterator(new RecursiveDirectoryIterator(VENDOR_PATH.'/bower_components/densajs/src/overrides'), RecursiveIteratorIterator::LEAVES_ONLY);
         foreach ($it as $i) {
             if (substr($i->getPathname(), -3) != '.js') continue;
-            $depName = 'Densa.'.str_replace('/', '.', substr($i->getPathname(), strlen(VENDOR_PATH.'/koala-framework/densajs/src/'), -3));
+            $depName = 'Densa.'.str_replace('/', '.', substr($i->getPathname(), strlen(VENDOR_PATH.'/bower_components/densajs/src/'), -3));
             $fileContents = file_get_contents($i->getPathname());
 
             // remove comments to avoid dependencies from docs/examples
