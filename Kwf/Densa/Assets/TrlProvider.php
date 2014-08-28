86541bb91c410cd2cb470843ae673b64d3faf24a
Niko Sams <ns@vivid-planet.com>
2014-08-28 08:16:42 +0200

    load trl dependency only once by re-using the same object

--- Kwf/Densa/Assets/TrlProvider.php
+++ Kwf/Densa/Assets/TrlProvider.php
@@ -4,9 +4,11 @@ class Kwf_Densa_Assets_TrlProvider extends Kwf_Assets_Provider_Abstract
     public function getDependenciesForDependency(Kwf_Assets_Dependency_Abstract $dependency)
     {
         if ($dependency instanceof Kwf_Densa_Assets_JsDependency) {
+            static $langDep;
+            if (!$langDep) $langDep = new Kwf_Assets_Dependency_File_Js('kwfdensa/densa-lang-en.js');
             return array(
                 Kwf_Assets_Dependency_Abstract::DEPENDENCY_TYPE_USES => array(
-                    new Kwf_Assets_Dependency_File_Js('kwfdensa/densa-lang-en.js')
+                    $langDep
                 )
             );
         }
